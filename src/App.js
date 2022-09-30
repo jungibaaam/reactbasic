97987aba7ab5a7f28d658b89aa8000d7e9a2b980
jungi <joongi1217@gmail.com>
2022-09-30 20:11:42 +0900

    Hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+// import Hello from './Hello';
 
 function App() {
+  const name = "Jungi"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1
+      style={{
+        color: "red",
+        backgroundColor: "green",
+      }}>
+        Hello, {name}.<p>{2 + 3}</p>
+     </h1>
+  </div>
   );
 }
 
